5d88eb66064bc36470cab7ec0792261ae9926442
Souop Sylvain Brayan <souopsylvain@gmail.com>
2025-07-23 09:52:42 +0100

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
+const Page = () => {
+  const [value, setValue] = useState("")
+
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({
+    onSuccess: () => {
+      toast.success("Background Job Started")
+    }
+  }));
+  
+  return (
+    <div className="p-4 max-w-7xl mx-auto">
+      <Input  value={value} onChange={(e) => setValue(e.target.value)}/>
+      <Button disabled={invoke.isPending} onClick={() => invoke.mutate({ value: value })}>
+        Invoke Background Job
+      </Button>
+    </div>
+  );
+}
+
+export default Page;
---
+++ src/inngest/functions.ts
@@ -0,0 +1,23 @@
+
+import {createAgent, openai } from "@inngest/agent-kit";
+import { inngest } from "./client";
+
+export const helloWorld = inngest.createFunction(
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert next.js developer.  You write readable, maintanable code, you write simple next.js and react snippets.",
+      model: openai({ model: "gpt-4o"}),
+    });
+
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`,
+    );
+
+
+    return { output}
+
+  },
+);
