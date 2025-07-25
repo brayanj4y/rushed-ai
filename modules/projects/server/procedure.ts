20e8d6264a558e6446edd6a36e6e3035d1ebfe43
Souop Sylvain Brayan <souopsylvain@gmail.com>
2025-07-25 16:13:14 +0100

    feat: implement messaging functionality with project association
    - Added MessageCard and MessageForm components for displaying and submitting messages.
    - Enhanced messages router to filter messages by project ID.
    - Integrated react-textarea-autosize for improved message input experience.
    - Created MessagesContainer to manage message display and form submission.
    - Introduced ProjectView to encapsulate project-specific messaging functionality.
    - Removed unused SVG files and added a new logo SVG.

--- modules/projects/server/procedure.ts
+++ modules/projects/server/procedure.ts
@@ -3,8 +3,30 @@ import { prisma } from "@/lib/db";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import { z } from "zod";
 import { generateSlug } from "random-word-slugs"
+import { TRPCError } from "@trpc/server";
+
+
 
 export const projectsRouter = createTRPCRouter({
+    getOne: baseProcedure
+        .input(z.object({
+            id: z.string().min(1, { message: "ID is required" }),
+        }))
+        .query(async ({ input }) => {
+            const existingProject = await prisma.project.findUnique({
+                where: {
+                    id: input.id,
+                },
+            });
+
+
+            if (!existingProject) {
+                throw new TRPCError({ code: "NOT_FOUND", message: "Project not found" });
+            }
+            
+            return existingProject;
+        }),
+
     getMany: baseProcedure
         .query(async () => {
             const projects = await prisma.project.findMany({
@@ -19,8 +41,8 @@ export const projectsRouter = createTRPCRouter({
         .input(
             z.object({
                 value: z.string()
-                .min(1, { message: "Value is required" })
-                .max(10000, { message: "Value is too Long"})
+                    .min(1, { message: "Value is required" })
+                    .max(10000, { message: "Value is too Long" })
             }),
         )
         .mutation(async ({ input }) => {
