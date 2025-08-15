e1746f37e27bfa38714175597bc82f2043b4e9eb
Souop Sylvain Brayan <souopsylvain@gmail.com>
2025-08-15 18:26:10 +0100

    refactor(projects): remove unnecessary class
    - Removed `scrollbar-hide` class.
    - Improved scrollbar visibility.

--- src/modules/projects/ui/components/messages-container.tsx
+++ src/modules/projects/ui/components/messages-container.tsx
@@ -56,8 +56,8 @@ export const MessagesContainer = ({
   const isLastMessageUser = lastMessage?.role === "USER";
 
   return (
-    <div className="flex flex-col flex-1 min-h-0">
-      <div className="flex-1 min-h-0 overflow-y-auto scrollbar-hide">
+    <div className="flex flex-col flex-1 min-h-0 scrollbar-hide">
+      <div className="flex-1 min-h-0 overflow-y-auto">
         <div className="pt-2 pr-1">
           {messages.map((message) => (
             <MessageCard
