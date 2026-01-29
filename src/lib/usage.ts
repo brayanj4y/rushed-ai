7334c3d8b7c2a9569ad662ab9d21a3073554151f
souop silvain brayan <souopsylvain@gmail.com>
2026-01-29 04:02:21 +0100

    Revert "fix: update usage limit per day to minimize bills"
    This reverts commit 10e3979104f182528e411412fc5a092de46ad779.

--- src/lib/usage.ts
+++ src/lib/usage.ts
@@ -2,10 +2,10 @@ import { RateLimiterPrisma } from "rate-limiter-flexible";
 import { prisma } from "./db";
 import { auth } from "@clerk/nextjs/server";
 
-const FREE_DAILY_LIMIT = 2;
-const PRO_DAILY_LIMIT = 5;
-const DURATION = 24 * 60 * 60;
-const GENERATION_COST = 0.5;
+const FREE_POINTS = 2;
+const PRO_POINTS = 100;
+const DURATION = 30 * 24 * 60 * 60; // 30 days
+const GENERATION_COST = 1;
 
 export async function getUsageTracker() {
   const { has } = await auth();
@@ -14,7 +14,7 @@ export async function getUsageTracker() {
   const usageTracker = new RateLimiterPrisma({
     storeClient: prisma,
     tableName: "Usage",
-    points: hasProAccess ? PRO_DAILY_LIMIT : FREE_DAILY_LIMIT,
+    points: hasProAccess ? PRO_POINTS : FREE_POINTS,
     duration: DURATION,
   });
 
